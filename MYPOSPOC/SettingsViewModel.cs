7e9b65be5b24e7f9b4e74c68477c2d94cf2d80c0
Chris <cdesch@gmail.com>
2017-06-11 12:10:42 -0400

    SettingsView

--- MYPOSPOC/SettingsViewModel.cs
+++ MYPOSPOC/SettingsViewModel.cs
@@ -9,64 +9,6 @@ namespace MYPOSPOC
 {
     class SettingsViewModel : ViewModelBase
     {
-        // public Settings settings { get; set; }
-
-
-        string _PrinterName;
-
-        public string PrinterName
-        {
-            get
-            {
-                return _PrinterName;
-            }
-            set
-            {
-                if (_PrinterName != value)
-                {
-                    _PrinterName = value;
-                    Properties.Settings.Default.PrinterName = _PrinterName;
-                    Console.WriteLine("PrinterNameChanged");
-                    RaisePropertyChanged("PrinterName");
-                }
-            }
-        }
-
-
-        string _FirstName;
-
-        public string FirstName
-        {
-            get
-            {
-                return _FirstName;
-            }
-            set
-            {
-                if (_FirstName != value)
-                {
-                    _FirstName = value;
-                    RaisePropertyChanged("FirstName");
-                }
-            }
-        }
-
-        string _LastName;
-        public string LastName
-        {
-            get
-            {
-                return _LastName;
-            }
-            set
-            {
-                if (_LastName != value)
-                {
-                    _LastName = value;
-                    RaisePropertyChanged("LastName");
-                }
-            }
-        }
 
         private Settings _settings;
         public Settings CurrentSettings
